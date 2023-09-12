852b8e3f4322192935c65e798639b8fa840717aa
frankid1412 <shouzhi.fang@blackcowtech.com>
2023-09-12 14:08:31 -0400

    update backendpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
-const backendEndpoint = process.env.REACT_APP_API_ENDPOINT;
+// const backendEndpoint = process.env.REACT_APP_API_ENDPOINT;
+const backendEndpoint = "http://sudok-appli-3g4f83isvbb5-1738922423.us-east-2.elb.amazonaws.com:8080/SpringBootSudokuExampleApplication";
 
 const config = {
   BACKEND_ENDPOINT: backendEndpoint
