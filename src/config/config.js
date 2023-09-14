14dd6e1e2b1496310785ce9df5ba2210af971494
frankid1412 <shouzhi.fang@blackcowtech.com>
2023-09-14 15:55:52 -0400

    update api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 // const backendEndpoint = process.env.REACT_APP_API_ENDPOINT;
-const backendEndpoint = "http://sudok-Appli-1HUJP6G5IMUNB-219774285.us-east-2.elb.amazonaws.com:8080";
+const backendEndpoint = "http://sudok-Appli-13R16808AJPZF-1815047026.us-east-2.elb.amazonaws.com:8080";
 
 const config = {
   BACKEND_ENDPOINT: backendEndpoint
