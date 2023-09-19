410de7230e834e01de6c5d21f81dd2eeb93110fd
frankid1412 <shouzhi.fang@blackcowtech.com>
2023-09-19 17:37:21 -0400

    revert

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 // const backendEndpoint = process.env.REACT_APP_API_ENDPOINT;
-// const backendEndpoint = "https://sudok-Appli-2QLTDOSBYUOM-1666661737.us-east-2.elb.amazonaws.com:443/";
-const backendEndpoint = "https://api.blackcowtechtest.cloud:443";
+const backendEndpoint = "sudok-Appli-2QLTDOSBYUOM-1666661737.us-east-2.elb.amazonaws.com:443/";
 
 const config = {
   BACKEND_ENDPOINT: backendEndpoint
