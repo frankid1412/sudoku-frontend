5b4cf8269814da2cb9c69f4e48b033e1346aa4f6
frankid1412 <shouzhi.fang@blackcowtech.com>
2023-09-22 17:14:01 -0400

    change backendendpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 // const backendEndpoint = process.env.REACT_APP_API_ENDPOINT;
-const backendEndpoint = "https://ec2.blackcowtechtest.cloud:443";
+const backendEndpoint = "https://api.blackcowtechtest.cloud:443";
 // const backendEndpoint = "https://sudok-Appli-1RHLTV7BW2Q89-432843480.us-east-2.elb.amazonaws.com:443"
 
 const config = {
