2036052c8c13c87ac01d3f7c4aa14b26167691ab
frankid1412 <shouzhi.fang@blackcowtech.com>
2023-09-28 16:13:54 -0400

    update backend endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 // const backendEndpoint = process.env.REACT_APP_API_ENDPOINT;
-const backendEndpoint = "https://www.blackcowtech.info:443";
+// const backendEndpoint = "https://www.blackcowtech.info:443";
+const backendEndpoint = "https://sudok-Appli-1WCLAWCFHJ3EH-1035841793.us-east-2.elb.amazonaws.com:443";
 
 const config = {
   BACKEND_ENDPOINT: backendEndpoint
